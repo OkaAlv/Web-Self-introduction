29c269dbde80601113e53d56aa314d18dbc72d6c
unknown <OkaAlv>
2024-10-19 14:52:27 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Kelas PSIK 23B",
+    "Fakultas Matematika dan Ilmu Pengetahuan Alam",
+    "UNIVERSITAS NEGERI MEDAN",
+  ],
+  typeSpeed: 80,
+  backSpeed: 30,
+  BackDelay: 1000,
+  loop: true,
+});
